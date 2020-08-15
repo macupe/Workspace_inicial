8b250fae5ca1e87e03b931e067269633f3680073
macupe110 <68623802+macupe110@users.noreply.github.com>
2020-08-15 19:54:21 -0300

    Entrega 1 finalizada, falta mejorar codigo, para hacerlo mas eficiente, pero cumple con los requerimientos solicitados

--- js/login.js
+++ js/login.js
@@ -5,10 +5,51 @@
 
     /*==================================================================
     [ dirigimos al index ]*/    
+//funcion para mostrar contrasenia
+var showPass = 0;
+    
+function showPasss(){
+    var pass = document.getElementById('pass'); 
+    if(showPass == 0) {
+             pass.setAttribute('type','text');
+             pass.setAttribute('class','zmdi-eye-off');
+             pass.setAttribute('class','zmdi-eye');
+             showPass = 1;
+         }
+         else {               
+             pass.setAttribute('type','password');
+             pass.setAttribute('class','zmdi-eye');
+             pass.setAttribute('class','zmdi-eye-off');
+             showPass = 0;
+         }
+     }
+
+  //funcion para validar que los campos sean correctos de lo contrario muestra una alerta
+function validarUser(){
+     var mail = document.getElementById('email');
+     var pass = document.getElementById('pass');
+     var inputDiv = document.getElementsByClassName('wrap-input100 validate-input');
+
+  if(!(/\S+@\S+\.\S+/.test(mail.value))){ 
+    inputDiv[0].classList.add('alert-validate');
+    return false;
+    }
+    if(pass.value == null || pass.value.length == 0 || /^\s+$/.test(pass.value)){
+      inputDiv[1].classList.add('alert-validate');
+      return false;
+    }
+    location.href='./primary.html';
+  }
+    
+function hideAlertM(){
+  document.getElementsByClassName('wrap-input100 validate-input')[0].classList.remove('alert-validate');
+}
+function hideAlertP(){
+  document.getElementsByClassName('wrap-input100 validate-input')[1].classList.remove('alert-validate');
 }
 
     document.addEventListener("DOMContentLoaded", function(e){
     
-
+     
 
     });
